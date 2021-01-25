f5b24eb982db7dffb3e74fa22516d4c6ee2f0d72
Florencia Parra <fnparra@gmail.com>
2021-01-24 22:21:00 -0300

    Se agregaron fotos al home

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const app = express();
 const path = require("path");
 
 
-app.listen(3000, console.log("Escuchando en el puerto 3000"));
 
-
-const staticPath = path.resolve(__dirname, "/public");
+const staticPath = path.resolve(__dirname, "./public");
 app.use(express.static(staticPath));
 
+app.listen(3000, console.log("Escuchando en el puerto 3000"));
 
 app.get("/", (req, res)=> {
     res.sendFile(path.resolve(__dirname, "./Views/home.html"))
