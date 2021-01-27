7a847f2427f16073c118ab234f1d7f1f6b6b2267
Florencia Parra <fnparra@gmail.com>
2021-01-27 00:48:47 -0300

    Modificacion css footer. Finalizo seccion productos del home

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ app.get("/productDetail", (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./Views/productDetail.html"))
 });
 
-app.get("./productCart", (req, res)=>{
+app.get("/productCart", (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./Views/productCart.html"))
 });
