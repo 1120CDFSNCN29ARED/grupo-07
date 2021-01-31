8adc52aa9c1387f1d1c2bd16d441c1569c17a03a
Lucia Pitrelli <pitrelliblucia@gmail.com>
2021-01-31 14:32:01 -0300

    agrego el head al logIn.html + vinculo home.html y app.js al logIn.html

--- app.js
+++ app.js
@@ -7,18 +7,22 @@ app.listen(3000, console.log("Escuchando en el puerto 3000"));
 const staticFolder = path.resolve(__dirname, "./public");
 app.use(express.static(staticFolder));
 
-app.get("/", (req, res)=> {
-    res.sendFile(path.resolve(__dirname, "./Views/home.html"))
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./Views/home.html"));
 });
 
-app.get("/register", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "./Views/register.html"))
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./Views/register.html"));
 });
 
-app.get("/productDetail", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "./Views/productDetail.html"))
+app.get("/productDetail", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./Views/productDetail.html"));
 });
 
-app.get("/productCart", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "./Views/productCart.html"))
+app.get("/productCart", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./Views/productCart.html"));
+});
+
+app.get("/logIn", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./Views/logIn.html"));
 });
