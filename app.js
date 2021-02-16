47515379156526a774083859b57ba196548a7131
Lucia Pitrelli <pitrelliblucia@gmail.com>
2021-02-16 04:46:48 -0300

    agrego reseña y help

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ app.listen(3000, console.log("Escuchando en el puerto 3000"));
 const staticFolder = path.resolve(__dirname, "./public");
 app.use(express.static(staticFolder));
 
-
+app.get("/ayuda", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/ayuda.html"));
+});
 
 app.use("/products", productsRoutes);
 
