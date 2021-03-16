f26f02e842ba5559469bbfa6c60f11ff10311158
Florencia Parra <fnparra@gmail.com>
2021-03-15 23:20:38 -0300

    Modificacion de registro, rutas y controllers de usuario

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,28 @@
+const { render }= require("ejs");
+const fs = require("fs");
+const path = require("path");
+
+const usersFilePath = path.join(__dirname, "../data/users.json");
+const usersJson = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+
 const users = {
   register: (req, res) => {
     res.render("users/register");
+  },
+  newUser: (req, res) =>{
+    let newRegister ={
+      id,
+      nombre: req.body.name,
+      apellido: req.body.surname,
+      foto: req.body.picture,
+      email: req.body.email,
+      contraseña: req.body.password
+    }
+    usersJson.push(newRegister);
+
+    fs.writeFileSync(usersFilePath, JSON.stringify(usersJson));
+
+    res.redirect("/")
   },
   login: (req, res) => {
     res.render("users/logIn");
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,29 @@
 
 const express = require("express");
 const router = express.Router();
+const path = require("path");
+const multer = require("multer");
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        const folder = path.resolve(__dirname, "../../public/img/users");
+        if (file.mimetype != "image/jpeg"){
+            return cb (new Error("Solo se aceptan archivos jpg o png"));
+        }
+        cb(null, folder);
+    },
+    filename: (req, file, cb) =>{
+        const newFilename = Date.now() + path.extname(file.originalname);
+        cb(null, newFilename);
+    }
+})
+
+const upload = multer ({ storage });
 // ************ Controller Require ************
 const users = require("../controllers/users");
 
-router.get("/register", users.register);
+router.get("/register",  users.register);
+router.post("/register", upload.single("picture"), users.newUser);
 
 router.get("/logIn", users.login);
 
